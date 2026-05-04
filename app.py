fb92fb0c3cc223bd9f4b901f5921cdee0ad4673c
Barath R K <barathkumarrk3010@gmail.com>
2026-05-04 14:11:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import tempfile
 import os
 
-# LangChain imports
+# CORRECT IMPORTS - using langchain_community
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
